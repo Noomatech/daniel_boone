bc72eb5d034e445596feb0be77f5c1b365fc2487
donald-s <don@parablesoft.com>
2015-12-16 11:33:38 -0500

    Walking skeleton.

--- spec/acceptance/daniel_boone_spec.rb
+++ spec/acceptance/daniel_boone_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 RSpec.describe 'daniel_boone', type: :aruba do
   it 'gets info from a GitHub repo' do
+    system 'export PATH=/Users/donny/Desktop/Development/Ruby/daniel_boone/bin/daniel_boone:$PATH'
+
     run_simple 'daniel_boone'
 
-    expect(all_output).to eq('')
+    expect(all_output).to eq('Test')
   end
 end
